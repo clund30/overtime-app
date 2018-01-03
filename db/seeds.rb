51bae904e1024ab53741261af4c2afdd40e019a0
Casey Lund <caseylund@Caseys-MBP.ad.uvu.edu>
2018-01-03 10:28:55 -0700

    Added twitter bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+User.create(email: "test@test.com",
+            password: "test123",
+            password_confirmation: "test123",
+            first_name: "Fake",
+            last_name: "User"
+            )
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content")
 end
