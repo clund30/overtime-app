467193d4cc7d2321f67466573ef6c897c8ef66fe
Casey Lund <caseylund21@gmail.com>
2018-01-10 10:37:44 -0700

    Implemented radio buttons for approved and rejected status on form page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,16 @@
 
 puts "1 User created"
 
-100.times do |post|
+AdminUser.create(email: "admin@test.com", 
+            password: "asdfasdf", 
+            password_confirmation: "asdfasdf", 
+            first_name: "Admin", 
+            last_name: "Name")
+
+puts "1 Admin User created"
+
+15.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
-puts "100 Posts have been created"
+puts "15 Posts have been created"
