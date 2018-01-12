14b65a07828f751e11336b8b24ca1fd79608caf9
Casey Lund <caseylund21@gmail.com>
2018-01-12 10:41:55 -0700

    added overtime hours request option

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ AdminUser.create(email: "admin@test.com",
 puts "1 Admin User created"
 
 15.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "15 Posts have been created"
