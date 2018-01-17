54e1e171eba5013b9470a58d0d517ff1b1b527e8
Casey Lund <caseylund21@gmail.com>
2018-01-17 14:13:31 -0700

    implemented phone validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,8 @@
             password: "test123",
             password_confirmation: "test123",
             first_name: "Jon",
-            last_name: "Snow"
-            phone: "8013168737"
-            )
+            last_name: "Snow",
+            phone: "8013168737")
 
 puts "1 User created"
 
@@ -12,9 +11,8 @@ AdminUser.create(email: "admin@test.com",
             password: "asdfasdf", 
             password_confirmation: "asdfasdf", 
             first_name: "Admin", 
-            last_name: "Name"
-            phone: "8013168737"
-            )
+            last_name: "Name",
+            phone: "8013168737")
 
 puts "1 Admin User created"
 
