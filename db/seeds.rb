d40e5ea0a06c52dc71a0840d4786406ed6237a98
Casey Lund <caseylund21@gmail.com>
2018-01-18 15:21:02 -0700

    added audit logs to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,9 @@ puts "1 Admin User created"
 end
 
 puts "15 Posts have been created"
+
+15.times do |audit_log|
+      AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "15 audit logs have been created"
